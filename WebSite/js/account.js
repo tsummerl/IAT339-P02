e8cc4bbf68b70fdc1a4e4e1bb2258d86e63ba500
T <twisummerland@gmail.com>
2017-10-24 21:26:37 -0700

    Updated account navigation

--- WebSite/js/account.js
+++ WebSite/js/account.js
@@ -3,6 +3,8 @@ $(document).ready(function(){
     var tabContent, tabLinks;
     tabContent = $('.tabContent').css("display", "none");
     tabLinks = $(".buttonNav.accountButtonNav");
+    $("#tabCurr").addClass("activeNavButton");
+    $("#myCurrBox").css("display", "block");
     tabLinks.click(function(){
         var id = $(this).attr("id");
         console.log(id);
@@ -22,8 +24,6 @@ $(document).ready(function(){
             break;
         }
         tabLinks.removeClass("activeNavButton");
-        tabLinks.parent().removeClass("activeNavButton");
         $(this).addClass("activeNavButton");
-        $(this).parent().addClass("activeNavButton");
     });
 });
